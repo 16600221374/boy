1cb47ec96dd175afb13b60a039bfbda4f82ef8ac
liuzuncai <2766033991@qq.com>
2023-05-10 14:14:58 +0800

    网关改为spring gateway

--- ZuulDemo/src/main/java/org/example/ZuulDemoApplication.java
+++ ZuulDemo/src/main/java/org/example/ZuulDemoApplication.java
@@ -3,13 +3,13 @@ package org.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+//import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
  * Hello world!
  *
  */
-@EnableZuulProxy
+//@EnableZuulProxy
 @EnableEurekaClient
 @SpringBootApplication
 public class ZuulDemoApplication
